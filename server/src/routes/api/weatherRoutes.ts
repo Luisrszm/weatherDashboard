050132b9584a5b1f3876e1c65d985d8f893ab372
Admin <admin@Major-Tom-2.local>
2025-03-10 20:09:00 -0600

    update the weather Services

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (_req, res) => {
   // TODO: GET weather data from city name
-  
+  res.
   // TODO: save city to search history
 });
 
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -4,19 +4,33 @@ dotenv.config();
 // TODO: Define an interface for the Coordinates object
 interface Coordinates {
   longitude: number;
-  altitude: number;
+  latitude: number;
 }
 
 // TODO: Define a class for the Weather object
 class Weather {
-  
+  coord: Coordinates[];
+  temp: number;
+  wind: number;
+  humidity: number;
+
+  constructor(coord: Coordinates[], temp: number, wind: number, humidity: number){
+    this.coord = coord
+    this.temp = temp
+    this.wind = wind
+    this.humidity = humidity
+  }
 }
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL = process.env.API_BASE_URL
+  private API_KEY = process.env.API_KEY
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+    const url = `${this.baseURL}/data/2.5/forecast?lat={lat}&lon={lon}&appid=${this.API_KEY}`
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method

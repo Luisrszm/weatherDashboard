f7d675d22b5233f55f2823ffb4aa91fc599664cf
Admin <admin@Major-Tom-2.local>
2025-03-11 21:41:18 -0600

    starts well

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
+// import path from 'node:path';
 
 dotenv.config();
 // Import the routes
 import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 // Serve static files from the 'dist' folder
-app.use(express.static('dist'));
-app.use('/db', express.static(path.join(__dirname, 'db')));
+app.use(express.static('../client/dist'));
 // Middleware to parse JSON payloads
 app.use(express.json());
 // Middleware to parse URL-encoded form data
